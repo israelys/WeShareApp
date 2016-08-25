1ec5ab22c0c487f14cbabb8204c058eacedca544
DavidD <diskind10@gmail.com>
2016-08-25 23:51:06 +0300

    New offer window
    הוספתי חלון להוספת הצעה חדשה,
    הוספתי שני חלונות, אחד לשפות משמאל לימין ואחד לעברית - מימין לשמאל

--- app/src/main/java/com/mla/israels/weshare/NewResponseActivity.java
+++ app/src/main/java/com/mla/israels/weshare/NewResponseActivity.java
@@ -3,13 +3,29 @@ package com.mla.israels.weshare;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mla.israels.weshare.DataObjects.Offer;
+import com.mla.israels.weshare.DataObjects.Request;
+import com.mla.israels.weshare.communication.OfferService;
+import com.mla.israels.weshare.communication.RestService;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by david on 15/08/2016.
  */
-public class NewResponseActivity extends Activity {
-    TextView responce_id;
+public class NewResponseActivity extends Activity implements View.OnClickListener {
+    Button btnSendOffer;
+    EditText etComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,28 @@ public class NewResponseActivity extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width * .8), (int) (height * .8));
+        btnSendOffer = (Button) findViewById(R.id.btn_sned_offer);
+        btnSendOffer.setOnClickListener(this);
+        etComment = (EditText) findViewById(R.id.offer_comment);
+    }
 
-        responce_id = (TextView) findViewById(R.id.request_id);
-        responce_id.setText(getIntent().getStringExtra("ID"));
+    @Override
+    public void onClick(View v) {
+        Offer offer = new Offer();
+        offer.Comment = etComment.getText().toString();
+        offer.RequestId = getIntent().getIntExtra("REQUEST_ID", -1);
+        offer.UserId = getIntent().getIntExtra("USER_ID", -1);
+        RestService.getInstance().getOfferService().addOffer(new Offer(), new Callback<Offer>() {
+            @Override
+            public void success(Offer offer, Response response) {
+                Toast.makeText(getApplicationContext(), "Success!...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(getApplicationContext(), "failed... " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
